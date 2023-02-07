e5307a6c081a5d96f29088591c358a9650a1f638
Hicaro Brasil <hicaro.brasil@hotmail.com>
2023-02-07 00:38:13 -0300

    :construction: build : sending script

---
+++ dowload.py
@@ -0,0 +1,32 @@
+from pytube import Youtube
+
+def printDowloadVideo():
+    global yt
+    print('dowloading' + yt.title)
+
+link:str = input("input link here: ")
+#Input a path 
+path = ''       
+
+yt = Youtube(link)
+stream = yt.strems
+while(True):
+    option = input('Press...'+
+    '1 - To download a video'+
+    '2 - To dowload a audio')
+
+    if option != '1' and option != '2':
+        print('select a valid option')
+        
+    
+    elif option =="1":
+        printDowloadVideo
+        stream = yt.stream.get_highest_resolution()
+        #Input a path
+        stream.dowload(output_path= path)
+        break
+    else:
+        printDowloadVideo
+        stream = yt.stream.get_audio_only('mp4')
+        stream.download(output_path= path)
+        break
